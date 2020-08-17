03650463ab9e16a10771aec8d5defa0ee1e8644e
OranBenAviFintek <69109203+OranBenAviFintek@users.noreply.github.com>
2020-08-17 11:10:24 +0300

    shopPage, preview and item
    updating our app with collection-preview, collection-item components and the shop page

--- src/App.js
+++ src/App.js
@@ -1,20 +1,15 @@
 import React from 'react';
 import { Switch, Route } from 'react-router-dom';
 import HomePage from './pages/homepage/homepage.component.jsx';
+import ShopPage from './pages/shop/shop.component.jsx';
 import './App.css';
-
-const Hatspage = () => (
-  <div>
-    <h1>Hats Page</h1>
-  </div>
-)
 
 function App() {
   return (
     <div className="App">
       <Switch>
         <Route  exact path='/' component={HomePage} />
-        <Route  path='/shop/hats' component={Hatspage} />
+        <Route  path='/shop' component={ShopPage} />
       </Switch>
     </div>
   );
