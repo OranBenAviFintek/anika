834eb211cf8b659f9302a59170740c48f4ea0568
OranBenAviFintek <69109203+OranBenAviFintek@users.noreply.github.com>
2020-08-18 10:52:46 +0300

    buttons and sign in with google

--- src/components/sign-in/sign-in.component.jsx
+++ src/components/sign-in/sign-in.component.jsx
@@ -44,14 +44,12 @@ class SignIn extends Component {
                     label="password"
                     handleChange={this.state.handleChange}
                     />
-
-                <CustomButton type="submit">Sign In</CustomButton>
-                <CustomButton onClick={signInWithGoogle}>
-                    {''}
-                    Sign In With Google
-                    {''}
-                </CustomButton>
-
+                <div className="buttons">
+                    <CustomButton type="submit">Sign In</CustomButton>
+                    <CustomButton onClick={signInWithGoogle} isGoogleSignIn>
+                        Sign In With Google
+                    </CustomButton>
+                </div>
             </form>
         </div>
       );
