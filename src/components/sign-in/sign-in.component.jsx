0d8178703b759510b85c0474bccb4d58a5e4d43e
OranBenAviFintek <69109203+OranBenAviFintek@users.noreply.github.com>
2020-08-19 23:42:58 +0300

    google button type=button

--- src/components/sign-in/sign-in.component.jsx
+++ src/components/sign-in/sign-in.component.jsx
@@ -59,7 +59,7 @@ class SignIn extends Component {
                 />
                 <div className="buttons">
                     <CustomButton type="submit">SIGN IN</CustomButton>
-                    <CustomButton onClick={signInWithGoogle} isGoogleSignIn>
+                    <CustomButton type="button" onClick={signInWithGoogle} isGoogleSignIn>
                         Sign In With Google
                     </CustomButton>
                 </div>
